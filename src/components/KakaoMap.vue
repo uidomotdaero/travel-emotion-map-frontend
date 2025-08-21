1287354cca59fee09dfc18c40d22a98f8360d461
Songjuho33 <songjh31@naver.com>
2025-08-22 06:03:16 +0900

    fix: GeoJSON 파일 경로 수정

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -213,7 +213,10 @@ export default {
 
     // 내 감정 데이터 로드 및 마커 생성 (기존 함수를 분리/수정)
     loadMyEmotions () {
-      apiClient.get('/api/travel-emotions')
+      // local
+      // apiClient.get('/api/travel-emotions')
+      // prod
+      axios.get('https://travel-emotion-map-backend.onrender.com/api/travel-emotions')
         .then(response => {
           this.createMarkers(response.data, 'my')
           // 여기서 마커를 지도에 표시해줘야 해!
